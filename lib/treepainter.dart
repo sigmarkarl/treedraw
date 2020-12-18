ee74cca39530e23337f9199f7f90a3b247c82cb9
Sigmar Stefansson <sigmar@hotmail.com>
2020-12-18 17:50:43 +0000

    add treepainter

---
+++ lib/treepainter.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class TreePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
